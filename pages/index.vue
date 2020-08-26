9da60e32b8d2dd2896e0d2c0538e10c4440d1c79
Max Hunter <maxwell.hunter@cardinalhealth.com>
2020-08-26 08:11:00 -0400

    more work

--- pages/index.vue
+++ pages/index.vue
@@ -31,14 +31,12 @@ export default {
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
-  word-spacing: 5px;
   padding-bottom: 15px;
 }
 
